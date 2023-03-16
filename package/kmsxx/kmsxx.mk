5452173ee6e3f18757798b7d53bdabd281bec010
Chris Shaw <cshaw@velo.ai>
2023-03-16 17:21:56 -0400

    Build python bindings for kmsxx

--- package/kmsxx/kmsxx.mk
+++ package/kmsxx/kmsxx.mk
@@ -4,17 +4,36 @@
 #
 ################################################################################
 
+################################################################################
+# Copilot specific
+#
+# 1. Build the python bindings.  In addition to enabling the option we have to
+# override Buildroot's desire to disable subproject wraps.
+# 2. The subproject wrap hits an https server for the pybind11 subproject and
+# we need to tell it where the certs are.
+################################################################################
 KMSXX_VERSION = 824bbb1f4cd062d66b457faca50f904b34dfd96c
 KMSXX_SITE = $(call github,tomba,kmsxx,$(KMSXX_VERSION))
 KMSXX_LICENSE = MPL-2.0
 KMSXX_LICENSE_FILES = LICENSE
 KMSXX_INSTALL_STAGING = YES
-KMSXX_DEPENDENCIES = fmt libdrm host-pkgconf
-KMSXX_CONF_OPTS = \
+KMSXX_DEPENDENCIES = fmt libdrm host-pkgconf python-pybind
+#KMSXX_CONF_OPTS = \
 	-Dkmscube=false \
 	-Dpykms=disabled \
 	-Domap=disabled \
 	-Dsystem-pybind11=enabled
+KMSXX_CONF_OPTS = \
+	-Dkmscube=false \
+	-Dpykms=enabled \
+	-Domap=disabled \
+	-Dsystem-pybind11=enabled \
+	--wrap-mode=default
+
+KMSXX_CONF_ENV += SSL_CERT_DIR=/etc/ssl/certs
+################################################################################
+# End Copilot specific
+################################################################################
 
 ifeq ($(BR2_TOOLCHAIN_HAS_GCC_BUG_85180),y)
 KMSXX_CXXFLAGS += $(TARGET_CXXFLAGS) -O0
